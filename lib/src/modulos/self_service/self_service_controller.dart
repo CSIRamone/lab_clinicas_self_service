193e25b967de26c3b84dbe6bb6e0bef4ab7539d7
csiramone <cidinei_inacio@hotmail.com>
2025-09-05 20:15:57 -0300

    AutoAtendimento(WhoIAm - Tela, adicionar dados no SelfServiceController, Reiniciar processo, desloga usuario

--- lib/src/modulos/self_service/self_service_controller.dart
+++ lib/src/modulos/self_service/self_service_controller.dart
@@ -1,15 +1,33 @@
+import 'package:fe_lab_clinicas_self_service/src/model/self_service_model.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 import 'package:signals_flutter/signals_flutter.dart';
 
 enum FormSteps { none, whoIAm, findPatient, patient, documents, done, restart }
 
 class SelfServiceController with MessageStateMixin {
+
+var _model = const SelfServiceModel();
+
   final _step = signal(FormSteps.none);
 
   FlutterSignal<FormSteps> get step => _step;
 
   void startProcess(){
     _step.value = FormSteps.whoIAm;
+  }
+
+  void setWhoIAmDataSetAndNext(String name, String lastName){
+    _model = _model.copyWith(name: () => name, lastName: () => lastName);
+    _step.value = FormSteps.findPatient;
+  }
+
+  void debug() {
+    print(_model.name);
+    print(_model.lastName);
+  }
+
+  void clearForm() {
+    _model = _model.clear();
   }
 
 }
